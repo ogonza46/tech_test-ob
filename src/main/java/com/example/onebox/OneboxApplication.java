79139a36a9820ce58486e5d19c7a63ef17dae71d
ogonza46 <32881332+ogonza46@users.noreply.github.com>
2023-06-01 10:07:27 +0200

    Add files via upload
    Initial Commit - clean project.

---
+++ src/main/java/com/example/onebox/OneboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.onebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneboxApplication.class, args);
+	}
+
+}
